ef6b9c26da553cc7e3096a74438d37254ca7a412
arno arend <arnoarend@icloud.com>
2023-05-30 14:41:30 +0200

    crud - create method done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get '/index', to: 'tasks#index'
-  get '/create', to: 'tasks#create'
+  root "tasks#index"
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # resources :tasks, only: [:index, :new, :create, :show]
 end
